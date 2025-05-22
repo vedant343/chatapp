c42b763d991d7318494befa4eae5c49416404a9a
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2025-05-22 15:43:30 +0530

    adding date in chats

--- src/app/chats/components/MessageList.tsx
+++ src/app/chats/components/MessageList.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 const MessageList = ({ messages, user, users = [] }) => {
+  let lastDisplayedDate = null;
+
   return (
     <div className="flex-1 overflow-y-auto px-4 py-2 space-y-2">
       {messages.length > 0 ? (
@@ -14,36 +16,56 @@ const MessageList = ({ messages, user, users = [] }) => {
             .toString()
             .padStart(2, "0")}`;
 
+          // Check if the date has changed
+          const displayDate = `${String(date.getDate()).padStart(
+            2,
+            "0"
+          )}-${String(date.getMonth() + 1).padStart(
+            2,
+            "0"
+          )}-${date.getFullYear()}`;
+          const showDate = lastDisplayedDate !== displayDate;
+          lastDisplayedDate = displayDate;
+
           // Find the sender's name based on sender_id
           const sender = users.find((u) => u.id === msg.sender_id);
 
           return (
-            <div
-              key={msg.id}
-              className={`flex ${
-                msg.sender_id === user?.id ? "justify-end" : "justify-start"
-              }`}
-            >
+            <div key={msg.id}>
+              {showDate && (
+                <div className="text-center text-gray-700 text-sm my-2 mx-100 bg-blue-100 p-1 rounded border-gray-500">
+                  {displayDate}
+                </div>
+              )}
               <div
-                className={`px-4 py-2 rounded-lg text-sm shadow ${
-                  msg.sender_id === user?.id
-                    ? "bg-[#d9fdd3] text-gray-800"
-                    : "bg-white text-gray-900 border"
+                className={`flex ${
+                  msg.sender_id === user?.id ? "justify-end" : "justify-start"
                 }`}
               >
-                <div className="flex justify-between">
-                  <span>
-                    {sender ? (
-                      <>
-                        <div className="text-xs text-gray-500">{sender.name}</div> <div>{msg.content}</div>{" "}
-                      </>
-                    ) : (
-                      <div>>{msg.content}</div>
-                    )}
-                  </span>
-                  <span className="text-xs text-gray-500 ml-2 text-right">
-                    {timeString}
-                  </span>
+                <div
+                  className={`px-3 py-2 rounded-lg text-sm shadow ${
+                    msg.sender_id === user?.id
+                      ? "bg-[#d9fdd3] text-gray-800"
+                      : "bg-white text-gray-900 border"
+                  }`}
+                >
+                  <div className="flex justify-between">
+                    <span>
+                      {sender ? (
+                        <>
+                          <div className="text-xs text-gray-500">
+                            {sender.name}
+                          </div>{" "}
+                          <div className="text-md">{msg.content}</div>{" "}
+                        </>
+                      ) : (
+                        <div className="text-xl">{msg.content}</div>
+                      )}
+                    </span>
+                    <span className="text-xs text-gray-500 ml-2 text-right">
+                      {timeString}
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
